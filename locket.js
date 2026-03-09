e441338704177ec4fc682106b18cd2bb69e90581
Nguyen Van An <86549983+CHIP2502@users.noreply.github.com>
2026-03-09 20:21:40 +0700

    Update locket.js

--- locket.js
+++ locket.js
@@ -5,45 +5,44 @@ const mapping = {
 
 var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
 var obj = JSON.parse($response.body);
+obj.Attention = "Enjoy";
 
-// Cấu hình dữ liệu "sạch" để hiện chữ "Đã là thành viên rồi"
 var subscriptionData = {
   is_sandbox: false,
   ownership_type: "PURCHASED",
   billing_issues_detected_at: null,
   period_type: "normal",
-  expires_date: null, // Để null để app hiện chữ mặc định
+  expires_date: "9999-09-09T00:00:00Z",
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
-  original_purchase_date: null,
-  purchase_date: null,
+  original_purchase_date: "9999-09-09T00:00:00Z",
+  purchase_date: "9999-09-09T00:00:00Z",
   store: "app_store"
 };
 
 var entitlementData = {
   grace_period_expires_date: null,
-  purchase_date: null,
+  purchase_date: "9999-09-09T00:00:00Z",
   product_identifier: "com.locket.Locket.premium.yearly",
-  expires_date: null
+  expires_date: "9999-12-18T01:04:17Z"
 };
 
 const match = Object.keys(mapping).find(e => ua.includes(e));
 
 if (match) {
   let [e, s] = mapping[match];
+  
   if (s) {
     entitlementData.product_identifier = s;
     obj.subscriber.subscriptions[s] = subscriptionData;
   } else {
     obj.subscriber.subscriptions["com.locket.Locket.premium.yearly"] = subscriptionData;
   }
+  
   obj.subscriber.entitlements[e] = entitlementData;
 } else {
   obj.subscriber.subscriptions["com.locket.Locket.premium.yearly"] = subscriptionData;
   obj.subscriber.entitlements.pro = entitlementData;
 }
 
-// Xóa triệt để các dấu vết ngày tháng cũ trong body response
-const finalBody = JSON.stringify(obj).replace(/"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z"/g, "null");
-
-$done({body: finalBody});
+$done({body: JSON.stringify(obj)});
