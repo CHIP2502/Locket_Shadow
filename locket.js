ce25310d70802a02bd5fac4cd60b719b593e4da7
Nguyen Van An <86549983+CHIP2502@users.noreply.github.com>
2026-03-09 19:58:11 +0700

    Update locket.js

--- locket.js
+++ locket.js
@@ -5,26 +5,31 @@ const mapping = {
 
 var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
 var obj = JSON.parse($response.body);
-obj.Attention = "Enjoy";
+
+// Thay đổi các giá trị chữ ở đây theo ý thích của bạn
+const chu_vinh_vien = "Zaara Dep Trai";
+const chu_ngay_mua = "Zaara Dep Trai";
+
+obj.Attention = "Gói cước đã được mở khóa!";
 
 var subscriptionData = {
   is_sandbox: false,
   ownership_type: "PURCHASED",
   billing_issues_detected_at: null,
   period_type: "normal",
-  expires_date: "9999-09-09T00:00:00Z",
+  expires_date: chu_vinh_vien, // Chuyển thành chữ
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
-  original_purchase_date: "9999-09-09T00:00:00Z",
-  purchase_date: "9999-09-09T00:00:00Z",
+  original_purchase_date: chu_ngay_mua, // Chuyển thành chữ
+  purchase_date: chu_ngay_mua, // Chuyển thành chữ
   store: "app_store"
 };
 
 var entitlementData = {
   grace_period_expires_date: null,
-  purchase_date: "9999-09-09T00:00:00Z",
+  purchase_date: chu_ngay_mua,
   product_identifier: "com.locket.Locket.premium.yearly",
-  expires_date: "9999-12-18T01:04:17Z"
+  expires_date: chu_vinh_vien
 };
 
 const match = Object.keys(mapping).find(e => ua.includes(e));
